451556ab4738481f94d318faf80d6c319f9407ef
Mossblac <mossblac.murphy@gmail.com>
2026-01-15 17:04:45 -0700

    eight byte chunks

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+	}
+
+	defer file.Close()
+
+	buffer := make([]byte, 8)
+	pickindex := 0
+
+	for {
+		bytesread, err := file.Read(buffer)
+
+		if bytesread > 0 {
+			chunk := buffer[:bytesread]
+			fmt.Printf("read: %s\n", string(chunk))
+			pickindex++
+		}
+
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			fmt.Printf("error: %v", err)
+		}
+	}
+
 }
